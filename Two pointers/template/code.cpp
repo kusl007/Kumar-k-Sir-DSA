86b81dfbbb504f11c139020fecf1716cfcc083b4
kusl007 <kushal.bansal.1001@gmail.com>
2025-03-06 22:49:46 +0545

    2 pointer template done

---
+++ Two pointers/template/code.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main() {
+    ll n;
+    cin>>n;
+    ll k;cin>>k;
+    ll b[n];
+    for(ll i=0;i<n;i++){
+        cin>>b[i];
+    }ll count = 0 ;
+    
+    
+    ll sum = 0 ;
+    for (int i = 0, j = 0; j < n; j++) {
+            sum = sum + b[j]; //[............]
+            while (sum>k){
+                sum = sum - b[i];
+                i++;
+            }
+            count += (j - i + 1);
+            
+            
+    }
+
+    cout<<count;
+    return 0;
+}//RRRRR
